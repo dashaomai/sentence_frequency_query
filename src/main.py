7172909578e30e5558e026d7d742d09496eea0e4
Bob Jiang <dashaomai@qq.com>
2018-01-05 08:39:51 +0000

    添加了多个查询条件，测试了大小写英文字符的正确查询

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@
 from db import sentence_frequency_query
 
 def main():
-    word = '曹操'
+    _query('曹操')
+    _query('耀C')
+    _query('耀c')
+
+def _query(word):
     frequency = sentence_frequency_query(word)
 
     print('查询：' + word + ' 获得以下结果：')
